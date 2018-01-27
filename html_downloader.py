805cf29bd175a2118f42c6ee8503e0e6a9298f57
KaenLee <35863189+Kaen-Lee@users.noreply.github.com>
2018-01-27 15:50:06 +0800

    baike_spider

---
+++ html_downloader.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+import string
+from urllib import request
+from urllib.parse import quote
+
+
+class HtmlDownloader(object):
+    def download(self, url):
+        if url is None:
+            return None
+
+        url_ = quote(url, safe=string.printable)
+        response = request.urlopen(url_)
+
+        if response.getcode() != 200:
+            return None
+
+        return response.read()
